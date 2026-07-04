1c1d607e3b85c823d72ffd232b3a7017bd233b65
hemab130613-code <hemab130613@gmail.com>
2026-07-04 21:57:48 +0530

    Fix public key

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import jwt
 app = FastAPI()
 
 PUBLIC_KEY = """
-PASTE THE PUBLIC KEY HERE EXACTLY AS GIVEN
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
 """
 
 ISSUER = "https://idp.exam.local"
@@ -18,7 +24,7 @@ class TokenRequest(BaseModel):
 @app.post("/verify")
 def verify_token(req: TokenRequest):
     try:
-        payload = jwt.decode(
+        payload = jwt.decode(S
             req.token,
             PUBLIC_KEY,
             algorithms=["RS256"],
