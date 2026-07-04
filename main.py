567fbd20db58c5a6842795a92085c2eccad539d0
hemab130613-code <hemab130613@gmail.com>
2026-07-04 22:02:37 +0530

    Fix JWT verification

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import jwt
 app = FastAPI()
 
 PUBLIC_KEY = """
+-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
 cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
 EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
@@ -13,18 +14,21 @@ WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
 ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
 SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
 dQIDAQAB
+-----END PUBLIC KEY-----
 """
 
 ISSUER = "https://idp.exam.local"
 AUDIENCE = "tds-o5hh1tel.apps.exam.local"
 
+
 class TokenRequest(BaseModel):
     token: str
+
 
 @app.post("/verify")
 def verify_token(req: TokenRequest):
     try:
-        payload = jwt.decode(S
+        payload = jwt.decode(
             req.token,
             PUBLIC_KEY,
             algorithms=["RS256"],
@@ -42,5 +46,5 @@ def verify_token(req: TokenRequest):
     except Exception:
         return JSONResponse(
             status_code=401,
-            content={"valid": False}
+            content={"valid": False},
         )
